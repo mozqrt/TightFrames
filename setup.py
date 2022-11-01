596962cd5d69ddc7044ccbb8058b6a5887d8fb1b
Maciej Żurawski home <maczur1999@gmail.com>
2022-11-01 19:25:48 +0100

    ind

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
- from setuptools import setup
+from setuptools import setup
 
- setup(
-   name='TightFrames',
-   version='0.1.0',
-   author='Maciej Żurawski',
-   author_email='mac@example.com',
-   packages=['numpy', 'scipy'],
-   scripts=['bin/script1','bin/script2'],
-   # url='http://pypi.python.org/pypi/PackageName/',
-   license='LICENSE.txt',
-   description='An awesome package that does something',
-   long_description=open('README.txt').read(),
-   install_requires=[
-       # "Django >= 1.1.1",
-       "pytest",
-   ],
-)
+setup(
+  name='TightFrames',
+  version='0.1.0',
+  author='Maciej Żurawski',
+  author_email='mac@example.com',
+  packages=['numpy', 'scipy'],
+  scripts=['bin/script1','bin/script2'],
+  # url='http://pypi.python.org/pypi/PackageName/',
+  license='LICENSE.txt',
+  description='An awesome package that does something',
+  long_description=open('README.txt').read(),
+  install_requires=[
+      # "Django >= 1.1.1",
+      "pytest",
+  ],)
